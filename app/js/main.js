aca5b705e656ccf51654dc4419a735ddc76a4d19
RomanVoz <113691287+RomanVoz@users.noreply.github.com>
2023-12-26 20:28:51 +0200

    правки

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,7 @@
 const menuBtn = document.querySelector('.menu__btn');
 const menuMobile = document.querySelector('.menu__list');
-const menuBtnClose = document.querySelector('.menu__btn--close');
 
 menuBtn .addEventListener('click', ()=> {
     menuMobile.classList.toggle('menu--open');
-    menuBtn.classList.toggle('menu--open');
-})
-
-menuBtnClose .addEventListener('click', ()=> {
-    menuBtnClose.classList.toggle('menu--open');
+    menuBtn.classList.toggle('menu--open')
 })
